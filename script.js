ab843fb7915c1c0115350ad1345f3a6a4defaf48
Kermishkov1996 <gtytr.n@yandex.ru>
2024-05-25 18:16:15 +0400

    ready select

--- script.js
+++ script.js
@@ -91,16 +91,43 @@ function validation(form) {
       removeError(inputCheckbox);
     }
 
-		if (input.value == "") {
-			removeError(input);
-			createError(input, "Поле не заполнено");
-			result = false;
-		}
-	}
-	
+    if (input.value == "") {
+      removeError(input);
+      createError(input, "Поле не заполнено");
+      result = false;
+    }
+  }
 
   return result;
 }
+
+let select = function () {
+  let selectHeader = document.querySelectorAll(".select__header");
+  let selectItem = document.querySelectorAll(".select_item");
+
+  selectHeader.forEach((item) => {
+    item.addEventListener("click", selectToggle);
+  });
+
+  selectItem.forEach((item) => {
+    item.addEventListener("click", selectChoose);
+  });
+
+  function selectToggle() {
+    this.parentElement.classList.toggle("is-active");
+  }
+
+  function selectChoose() {
+    let text = this.innerText;
+		let select = this.closest(".select")
+    let currentText = select.querySelector('.select__current');
+
+		currentText.innerText = text;
+		select.classList.remove('is-active')
+  }
+};
+
+select();
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
